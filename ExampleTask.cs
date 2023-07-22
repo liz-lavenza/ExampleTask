b907ee33453f2da088ea630632e343be9bd59c74
liz-lavenza <madeleine_is_not_my_real_name@protonmail.ch>
2023-07-22 01:23:15 -0500

    Add basic example task

---
+++ ExampleTask.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using TasksLib;
+
+namespace ExampleTask
+{
+    public class ExampleTaskMod : MelonMod
+    {
+        public override void OnLateInitializeMelon()
+        {
+            TasksLibMod.AddTask(new WaifuTask() {
+                StudentID = 40,
+                Description = "Mai Waifu wants some soda, but she's banned from using the vending machines. She'll give you $5 to buy one for her.",
+                Icon = "TaskSodaCan.png",
+                Lines = new string[6] {
+                    "Don't worry about it, Yan-chan, I'll be fine.", // player rejection
+                    "Well, see, I really want some soda...", // task introductions
+                    "But after the last time I tried to break into the vending machine, they won't even let me near it!",
+                    "Could you get some for me? I'll give you money!",
+                    "Here, this five dollar bill should cover it.", // player accept
+                    "Thank you so much, Yan-chan! Keep the change.", // task completed
+                }
+            });
+        }
+        class WaifuTask : YandereTask
+        {
+            protected override void OnMarkActive()
+            {
+                taskman.Yandere.Inventory.Money += 5;
+                taskman.Yandere.Inventory.UpdateMoney();
+            }
+            public override bool IsComplete()
+            {
+                return taskman.Yandere.Inventory.Soda;
+            }
+            public override void OnTurnedIn()
+            {
+                taskman.Yandere.Inventory.Soda = false;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExampleTask")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExampleTask")]
+[assembly: AssemblyCopyright("Copyright © Liz Lavenza 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96d06cca-542e-449f-ada5-ae77267809d9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(ExampleTask.ExampleTaskMod), "Example Task", "0.1", "Elizabeth Lavenza")]
+[assembly: MelonGame("YandereDev", "YandereSimulator")]
